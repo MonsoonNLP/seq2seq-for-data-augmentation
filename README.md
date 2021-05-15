c6dcdab2dbf80133c16cbafc065b4f0c5a3cdb83
Nick Doiron <ndoiron@mapmeld.com>
2021-05-14 21:04:04 -0600

    script and documentation

--- README.md
+++ README.md
@@ -2,4 +2,47 @@
 
 Kit of functions to help do this with datasets
 
-Open source, MIT license 
+## Features
+
+### Append rows to data
+
+Send the encoder and decoder model names from HuggingFace,
+the data as an array ```[[txt1, label1], [txt2, label2]]```,
+probability of flipping a row (default is 0.5),
+and whether to append an identical row (default is False)
+
+
+```python
+append_sequenced(
+    "monsoon-nlp/es-seq2seq-gender-encoder",
+    "monsoon-nlp/es-seq2seq-gender-decoder",
+    [["Bueno", 0], ["La biblioteca", 1], ["La maestra es tonta", 0]],
+    frequency=0.5,
+    always_append=False
+)
+```
+
+If randomly selected, the input ```["La maestra es tonta", 1]``` will result in ```["el maestro es tonto", 1]``` being appended to return data.
+
+If always_append=True, the "bueno" and "la biblioteca" rows
+will be included, unmodified.
+
+### Replace rows in data
+
+Send the encoder and decoder model names from HuggingFace,
+the data as an array ```[[txt1, label1], [txt2, label2]]```,
+and probability of flipping a row (default is 0.5)
+
+```python
+replace_sequenced(
+    "monsoon-nlp/es-seq2seq-gender-encoder",
+    "monsoon-nlp/es-seq2seq-gender-decoder",
+    [["Bueno", 0], ["La biblioteca", 0], ["La maestra es tonta", 1]],
+    frequency=0.5
+)
+```
+
+If randomly selected, the input ```["La maestra es tonta", 1]``` will be replaced with ```["el maestro es tonto", 1]```.
+
+
+Open source, MIT license
---
+++ seq2seqkit.py
@@ -0,0 +1,76 @@
+from random import random
+from transformers import AutoTokenizer, EncoderDecoderModel
+
+def prep(encoder_model, decoder_model):
+	tokenizer = AutoTokenizer.from_pretrained(model_name)
+	model = EncoderDecoderModel.from_encoder_decoder_pretrained(
+		encoder_model,
+		decoder_model,
+		max_length=40,
+	)
+	return tokenizer, model
+
+def generate(tokenizer, model, input_ids, input_labels, always_append):
+	sep_code = tokenizer.encode('')[-1]
+	myrows = []
+
+	batch = tokenizer(input_ids, return_tensors="pt", truncation=True, padding="longest")
+	generated = model.generate(batch.input_ids,
+							   decoder_start_token_id=model.config.decoder.pad_token_id)
+	g_dex = 0
+	for g in generated:
+		glist = g.tolist()
+		if sep_code in glist:
+			alt_txt = tokenizer.decode(glist[1 : glist.index(sep_code)])
+		else:
+			# the longest sequence of the batch (no [SEQ] token at end)
+			alt_txt = tokenizer.decode(glist[1 : ])
+		# rules on whether to add to final
+		if always_append or (alt_txt.lower().replace(' ', '') != input_ids[g_dex].lower().replace(' ', '')):
+			myrows.append([alt_txt, input_labels[g_dex]])
+		g_dex += 1
+	return myrows
+
+def apply_sequenced(pattern, encoder_model, decoder_model, trainrows, frequency, always_append):
+	if pattern == "append":
+		finalrows = trainrows
+	else:
+		finalrows = []
+	input_ids = []
+	input_txts = []
+	input_labels = []
+
+	tokenizer, model = prep(encoder_model, decoder_model)
+	for row in trainrows:
+		rowtxt = row[0]
+		rowlabel = row[1]
+
+		if random() < frequency:
+			# avoid sequences being too long
+			maxtxt = rowtxt.split(" ")
+			if len(maxtxt) > 400:
+				rowtxt = " ".join(maxtxt[:400])
+
+			# append to batch
+			input_ids.append(rowtxt)
+			input_labels.append(rowlabel)
+
+			if len(input_ids) > 150:
+				newrows = generate(tokenizer, model, input_ids, input_labels, always_append)
+				finalrows += newrows
+				input_ids = []
+				input_labels = []
+		elif pattern == "replace":
+			finalrows.append(row)
+	if len(input_ids) > 0:
+		# leftover from paging
+		newrows = generate(tokenizer, model, input_ids, input_labels, always_append)
+		finalrows += newrows
+
+	return finalrows
+
+def append_sequenced(encoder_model, decoder_model, trainrows, frequency=0.5, always_append=False):
+	return apply_sequenced("append", encoder_model, decoder_model, trainrows, frequency, always_append)
+
+def replace_sequenced(encoder_model, decoder_model, trainrows, frequency=0.5):
+	return apply_sequenced("replace", encoder_model, decoder_model, trainrows, frequency, True)
