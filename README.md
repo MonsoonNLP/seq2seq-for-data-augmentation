5b701ff99345e3b1ef09829a1b2fe28f12bffae1
Nick Doiron <ndoiron@mapmeld.com>
2021-07-06 11:56:11 -0600

    seq2seq update

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
 # seq2seq-for-data-augmentation
 
-Kit of functions to help modify a text dataset, with a seq2seq model
+Kit of functions to help modify a text dataset with a seq2seq model
 
 ## Features
 
 ### Append rows to data
 
 Send the encoder and decoder model names from HuggingFace,
-the data as an array ```[[txt1, label1], [txt2, label2]]```,
+the data as a DataFrame ```pd.DataFrame([[txt1, label1], [txt2, label2]], columns=['text', 'label'])```,
 model's maximum sequence length (default is 512),
 frequency of flipping a row (default is 0.5),
-and whether to append a row if it came out identical to the original (default is False)
+random_state (given to train_test_split),
+and whether to append a row if it comes out identical to the original (default is False)
 
 
 ```python
+initial_df = pd.DataFrame([["Bueno", 0], ["La biblioteca", 1], ["La maestra es tonta", 0]],
+  columns=['text', 'label']
+)
 append_sequenced(
     "monsoon-nlp/es-seq2seq-gender-encoder",
     "monsoon-nlp/es-seq2seq-gender-decoder",
-    [["Bueno", 0], ["La biblioteca", 1], ["La maestra es tonta", 0]],
+    initial_df,
     seq_length=512,
     frequency=0.5,
+    random_state=1,
     always_append=False
 )
 ```
 
-If randomly selected, the input ```["La maestra es tonta", 1]``` will result in ```["el maestro es tonto", 1]``` being appended to return data.
+If randomly selected, the input ```["La maestra es tonta", 1]``` will result in ```["el maestro es tonto", 1]``` being appended to the returned DataFrame.
 
 If always_append=True, the "bueno" and "la biblioteca" rows
 will be included, unmodified.
@@ -32,17 +37,22 @@ will be included, unmodified.
 ### Replace rows in data
 
 Send the encoder and decoder model names from HuggingFace,
-the data as an array ```[[txt1, label1], [txt2, label2]]```,
+the data as a DataFrame ```pd.DataFrame([[txt1, label1], [txt2, label2]], columns=['text', 'label'])```,
 model's maximum sequence length (default is 512),
-and frequency of flipping a row (default is 0.5)
+frequency of flipping a row (default is 0.5),
+and random_state (given to train_test_split)
 
 ```python
+initial_df = pd.DataFrame([["Bueno", 0], ["La biblioteca", 1], ["La maestra es tonta", 0]],
+  columns=['text', 'label']
+)
 replace_sequenced(
     "monsoon-nlp/es-seq2seq-gender-encoder",
     "monsoon-nlp/es-seq2seq-gender-decoder",
-    [["Bueno", 0], ["La biblioteca", 0], ["La maestra es tonta", 1]],
+    initial_df,
     seq_length=512
-    frequency=0.5
+    frequency=0.5,
+    random_state=12
 )
 ```
 
@@ -50,10 +60,16 @@ If randomly selected, the input ```["La maestra es tonta", 1]``` will be replace
 
 ## Applied
 
-Working with SimpleTransformers to see if appending or replacing rows
+(older syntax) working with SimpleTransformers to see if appending or replacing rows
 improves accuracy of classifiers:
 
 https://colab.research.google.com/drive/194ITDA1AjxAx_4ZLjoRFQI1aWzsl7xU8?usp=sharing
+
+## Dependencies
+
+```
+pip install pandas scikit-learn transformers
+```
 
 ## License
 
--- seq2seqkit.py
+++ seq2seqkit.py
@@ -1,4 +1,6 @@
 from random import random
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from transformers import AutoTokenizer, EncoderDecoderModel
 
 def prep(encoder_model, decoder_model, seq_length):
@@ -27,50 +29,48 @@ def generate(tokenizer, model, input_ids, input_labels, always_append):
 			alt_txt = tokenizer.decode(glist[1 : ])
 		# rules on whether to add to final
 		if always_append or (alt_txt.lower().replace(' ', '') != input_ids[g_dex].lower().replace(' ', '')):
-			myrows.append([alt_txt, input_labels[g_dex]])
+			myrows.append({ 'text': alt_txt, 'label': input_labels[g_dex] })
 		g_dex += 1
 	return myrows
 
-def apply_sequenced(pattern, encoder_model, decoder_model, trainrows, seq_length, frequency, always_append):
+def apply_sequenced(pattern, encoder_model, decoder_model, data_df, seq_length, frequency, random_state, always_append):
+	mod, keep = train_test_split(data_df, train_size=frequency, random_state=random_state)
 	if pattern == "append":
-		finalrows = trainrows.copy()
+		finalrows = data_df.copy()
 	else:
-		finalrows = []
-	input_ids = []
-	input_txts = []
-	input_labels = []
+		finalrows = keep
 
 	tokenizer, model = prep(encoder_model, decoder_model, seq_length)
-	for row in trainrows:
+	input_ids = []
+	input_labels = []
+	for row in mod.values:
 		rowtxt = row[0]
 		rowlabel = row[1]
 
-		if random() < frequency:
-			# avoid sequences being too long
-			maxtxt = rowtxt.split(" ")
-			if len(maxtxt) > 400:
-				rowtxt = " ".join(maxtxt[:400])
+		# avoid sequences being too long
+		maxtxt = rowtxt.split(" ")
+		if len(maxtxt) > 400:
+		  rowtxt = " ".join(maxtxt[:400])
 
-			# append to batch
-			input_ids.append(rowtxt)
-			input_labels.append(rowlabel)
+		# append to batch
+		input_ids.append(rowtxt)
+		input_labels.append(rowlabel)
 
-			if len(input_ids) > 150:
-				newrows = generate(tokenizer, model, input_ids, input_labels, always_append)
-				finalrows += newrows
-				input_ids = []
-				input_labels = []
-		elif pattern == "replace":
-			finalrows.append(row)
+		if len(input_ids) > 150:
+		  newrows = generate(tokenizer, model, input_ids, input_labels, always_append)
+		  finalrows = finalrows.append(newrows, ignore_index=True)
+		  input_ids = []
+		  input_labels = []
+
 	if len(input_ids) > 0:
 		# leftover from paging
 		newrows = generate(tokenizer, model, input_ids, input_labels, always_append)
-		finalrows += newrows
+		finalrows = finalrows.append(newrows, ignore_index=True)
 
 	return finalrows
 
-def append_sequenced(encoder_model, decoder_model, trainrows, seq_length=512, frequency=0.5, always_append=False):
-	return apply_sequenced("append", encoder_model, decoder_model, trainrows, seq_length, frequency, always_append)
+def append_sequenced(encoder_model, decoder_model, data_df, seq_length=512, frequency=0.5, random_state=0, always_append=False):
+	return apply_sequenced("append", encoder_model, decoder_model, data_df, seq_length, frequency, random_state, always_append)
 
-def replace_sequenced(encoder_model, decoder_model, trainrows, seq_length=512, frequency=0.5):
-	return apply_sequenced("replace", encoder_model, decoder_model, trainrows, seq_length, frequency, True)
+def replace_sequenced(encoder_model, decoder_model, data_df, seq_length=512, frequency=0.5, random_state=0):
+	return apply_sequenced("replace", encoder_model, decoder_model, data_df, seq_length, frequency, random_state, True)
